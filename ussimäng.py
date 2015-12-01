9bf5a76e978ccf6ff216fb0534cf6bf25d02be57
helen2po <helen2post@gmail.com>
2015-12-01 18:46:37 +0200

    Update ussimäng.py
    Add screen proberties

--- ussimäng.py
+++ ussimäng.py
@@ -10,3 +10,14 @@ must=(0,0,0)
 punane=(255,0,0)
 roheline=(0,155,0)
 helesinine=(240,248,255)
+
+#Ekraani mõõtmed
+ekraani_laius=800
+ekraani_korgus=600
+
+pygame.init()
+ekraan=pygame.display.set_mode((ekraani_laius,ekraani_korgus))
+ekraan.fill(valge)
+pygame.display.update()
+pygame.display.set_caption("Ussimäng")
+
