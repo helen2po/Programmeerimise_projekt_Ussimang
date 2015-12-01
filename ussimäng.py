7f643abf65c3873312f63eb9f173929e7fe7bb9f
adenosiin <liinapappa@hotmail.com>
2015-12-01 18:45:17 +0200

    Update ussimäng.py
    Add colour codes

--- ussimäng.py
+++ ussimäng.py
@@ -1,1 +1,12 @@
+# Programmeerimise_projekt_Ussimang
+# Kood programeerimise projekti jaoks nimega "Ussimäng".
 
+import pygame, random, sys
+from pygame.locals import *
+
+#Värvid
+valge=(255,255,255)
+must=(0,0,0)
+punane=(255,0,0)
+roheline=(0,155,0)
+helesinine=(240,248,255)
