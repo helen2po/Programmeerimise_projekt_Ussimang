5e43cca2eecc8adf527ae2ec82569502d5496c27
adenosiin <liinapappa@hotmail.com>
2015-12-02 19:47:42 +0200

    Update ussimäng.py
    Add minor changes

--- ussimäng.py
+++ ussimäng.py
@@ -1,6 +1,3 @@
-# Programmeerimise_projekt_Ussimang
-# Kood programeerimise projekti jaoks nimega "Ussimäng".
-
 import pygame, random, sys
 from pygame.locals import *
 
@@ -35,15 +32,15 @@ def mangu_algus():
 
             if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_s:
-                    intro=False
+                    algus=False
                 if event.key==pygame.K_q:
                     pygame.quit
                     quit()
-        prindi_ekraanile("Tere tulemast mängima ussimängu!", punane, -170, "suur")
+        prindi_ekraanile("Ussimäng", punane, -170, "suur")
         prindi_ekraanile("Selles mängus pead söötma ussi ja seeläbi teda kasvatama.", must, -90, "väike")
         prindi_ekraanile("Mängu alustamiseks vajuta ''s''", must, -65, "väike")
         prindi_ekraanile("Pausiks vajuta ''p''", must, -32, "väike")
-        prindi_ekraanile("ning mängu lõpetamiseks vajuta ''q''", must, -2, "väike")
+        prindi_ekraanile("Mängu lõpetamiseks vajuta ''q''", must, -2, "väike")
         prindi_ekraanile("Mäng saab läbi, kui põrkad ussiga vastu seina või vastu ussisaba", must, 50, "väike")
         prindi_ekraanile("Mängimiseks kasuta nooleklahve", must, 75, "väike")
         pygame.display.update()
@@ -61,4 +58,31 @@ def prindi_ekraanile(tekst, värv, y_nihe, suurus):
     teksti_valimus,valjastatud_tekst=muuda_teksti_suurus(tekst, värv, suurus)
     valjastatud_tekst.center=(ekraani_laius/2),(ekraani_korgus/2)+y_nihe
     ekraan.blit(teksti_valimus,valjastatud_tekst)
-    
+
+
+
+#def segment(x,y):
+#    
+#
+#    return()
+#segmendi_laius=10
+#segmendi_korgus=10
+#ussi_asukoht=[]
+#def uss():
+#    for i in range(4):
+#        ussi_x=400-segmendi_laius*i
+#        ussi_y=300
+#        ussi_segment=segment(ussi_x,ussi_y)
+#        ussi_asukoht.append(ussi_segment)
+
+#def toidu_asukoht():
+#    ussi_asukoht=[(x1,y1),(x2,y2)]
+#    while True:
+#        toidu_asukoht=(random.randint(0,800),random.randint(0,600))
+#        if toidu_asukoht not in ussi_asukoht:
+#            genereeri_toit(x,y)
+#            break
+
+#    toidu_x_koord=random.randint(0,600)
+#    toidu_y_koord=random.randint(0,800)
+#    return(toidu_x_koord,toidu_y_koord)
